ee0a943749c895b447eb66e0632093e47b973a87
live7374 <5358207@gmail.com>
2023-01-22 16:16:31 +0200

    sirting task in progress

--- index.js
+++ index.js
@@ -24,12 +24,60 @@ function transform (data) {
     return result.toFixed(2) //round number 
 }
 
-console.log(transform(task))
+//console.log(transform(task))
 
 ///extend functionality with additional values
 
 ratio["mm"] = 1000;
 ratio["yd"] = 1.094;
 ratio["km"] = 0.001;
+
+
+////////////////////////////////////////////////////////////////
+////Task 2
+
+const obj = {
+    data: [
+      { name: "John", email: "john2@mail.com" },
+      { name: "John", email: "john1@mail.com" },
+      { name: "Jane", email: "jane@mail.com" },
+    ],
+  };
+  
+  const test = {
+    condition: {
+      include: [{ name: "John" }],
+      sort_by: ["email"],
+    },
+  };
+  
+
+  let nameInclude;
+  for (let key in test.condition.include[0]) {
+    nameInclude = key;
+  }
+  
+  const dataName = test.condition.include[0][nameInclude];
+  const sortEmail = test.condition.sort_by[0];
+  console.log(dataName)
+  
+  const resultInclude = obj.data
+    .filter((o) => o[nameInclude] === dataName)
+
+ 
+  const resultSorted = resultInclude
+    .sort((a, b) => (b[sortEmail] > a[sortEmail] ? -1 : 1));
+  
+  const resultObj = { resultSorted };
+//   console.table(resultObj);
+
+////
+  let obj2 = {"data": [{"user": "mike@mail.com", "rating": 20, "disabled": false},
+  {"user": "greg@mail.com", "rating": 14, "disabled": false},
+  {"user": "john@mail.com", "rating": 25, "disabled": true}],
+  "condition": {"exclude": [{"disabled": true}], "sort_by": ["rating"]}};
+
+const dataStatus = obj2.condition.exclude[0].disabled;
+
 
 

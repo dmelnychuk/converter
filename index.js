e99d5a507f2160fa965b009c1026b01c30580aa9
live7374 <5358207@gmail.com>
2023-01-23 15:15:05 +0200

    task 3 binary search start

--- index.js
+++ index.js
@@ -107,3 +107,29 @@ const resultObj2 = { resultRating };
 
 console.log(resultObj2);
     
+
+
+
+///Task 3
+
+ console.log("Task 3...");
+
+ function binarySearch (arr, target) {
+    let leftIndex = 0
+    let rightIndex = arr.length - 1;
+
+    while (leftIndex <= rightIndex) {
+        let middleIndex = Math.floor(leftIndex + rightIndex) /2
+        if (target === arr[middleIndex]) {
+            return  middleIndex}
+            if(target  < arr[middleIndex]) {
+                rightIndex = middleIndex - 1
+            } else {
+                leftIndex = middleIndex + 1
+            }
+    }
+    return -1
+
+ }
+
+ console.log(binarySearch([3, 4, 5, 6, 7, 9, 17, 22, 21, 23], 4))
